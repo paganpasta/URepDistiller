ecbaf9c5fcaf427d8016748e59c2165504353ea6
unknown <aditya.91.singh@gmail.com>
2022-10-31 09:45:02 +0000

    name change of w_cos in loss file

--- distiller_zoo/CoSS.py
+++ distiller_zoo/CoSS.py
@@ -9,13 +9,13 @@ class CoSSLossPreN(nn.Module):
         self.t_temp = args.t_temp
         self.s_temp = args.s_temp
         self.w_0 = 1
-        self.w_1 = args.w_cos
+        self.w_1 = args.w_coss
         self.method = args.distill
 
     def forward(self, s_feat, t_feat):
         def dot_p(t_feat, s_feat):
-            tf = F.normalize(t_feat/self.t_temp, dim=-1, p=2) # / self.t_temp
-            sf = F.normalize(s_feat/self.s_temp, dim=-1, p=2) # / self.s_temp
+            tf = F.normalize(t_feat/self.t_temp, dim=-1, p=2)
+            sf = F.normalize(s_feat/self.s_temp, dim=-1, p=2)
             batchloss = -(tf * sf).sum(dim=-1)
             return batchloss
 
